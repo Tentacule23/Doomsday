1818dcbef05f06e583ca26d0b13b5b0cd988781c
Tentacule23 <mad_ol_man@hotmail.com>
2014-01-28 22:32:05 -0500

    28/01/2014 10:31pm

--- Doomsday/Doomsday.c
+++ Doomsday/Doomsday.c
@@ -29,21 +29,16 @@
 
 int dayOfWeek(int doomsday, int leapYear, int month, int day);
 int isLeapYear(int year);
-//int zedoomdate(int month);
+
 
 int day;
 int month;
 int year;
-
+int doomsday;
+int leapYear;
 
 int main(int argc, char *argv[]) {
 
-	printf("Enter Day(DD)\n");
-	scanf("%d", &day);
-	printf("Enter Month(MM)\n");
-	scanf("%d", &month);
-	printf("Enter Year(YYYY)\n");
-	scanf("%d", &year);
 
 	assert(dayOfWeek(THURSDAY, FALSE, 4, 4) == THURSDAY);
 	assert(dayOfWeek(FRIDAY, FALSE, 6, 6) == FRIDAY);
@@ -70,8 +65,23 @@ int main(int argc, char *argv[]) {
 	assert(dayOfWeek(TUESDAY, FALSE, 2, 28) == TUESDAY);
 	assert(dayOfWeek(TUESDAY, FALSE, 2, 27) == MONDAY);
 	assert(dayOfWeek(THURSDAY, FALSE, 1, 3) == THURSDAY);
+	
 
 	printf("all tests passed - You are awesome!\n");
+
+	printf("Enter Day(DD)\n");
+	scanf("%d", &day);
+	printf("Enter Month(MM)\n");
+	scanf("%d", &month);
+	printf("Enter Year(YYYY)\n");
+	scanf("%d", &year);
+
+	doomsday = (TUESDAY + year + (year / 4) - (year / 100) + (year / 400)) % 7;
+	printf("%d is doomsday\n", doomsday);
+	int isLeapYear(int year);
+	int dayOfWeek(int doomsday, int leapYear, int month, int day);
+	printf("%d is day of week", dayOfWeek);
+
 	return EXIT_SUCCESS;
 }
 
@@ -87,150 +97,13 @@ int dayOfWeek(int doomsday, int leapYear, int month, int day) {
 
 	// INSERT YOUR CODE HERE
 	
+	printf("%d is month\n", month);
+	printf("%d is day\n", day);
+	printf("New Check\n");
+	
 
-	doomsday = (TUESDAY + year + (year / 4) - (year / 100) + (year / 400))%7;
-	printf("%d is doomsday\n", doomsday);
-
-	//doomsday = 31/1, 28/2, 0/3, 4/4, 5/9, 6/6, 7/11, 8/8, 9/5, 10/10, 11/7, 12/12
+	//doomsday = 31/1(32ifleap), 28/2(29ifleap), 0/3, 4/4, 9/5, 6/6, 11/7, 8/8, 5/9, 10/10, 7/11, 12/12
 	//aka if doomsday = jeudi = le 4 avril est un jeudi
-
-	if (month == 1)
-	{
-		if (leapYear == TRUE)
-		{
-			doomsdate = 32;
-		}
-		else
-		{
-			doomsdate = 31;
-		}
-	}
-	if (month == 2)
-	{
-		if (leapYear == TRUE)
-		{
-			doomsdate = 29;
-		}
-		else
-		{
-			doomsdate = 28;
-		}
-	}
-	if (month == 3)
-	{
-		doomsdate = 0;
-	}
-	if (month = 4)
-	{
-		doomsdate = 4;
-	}
-	if (month == 5)
-	{
-		doomsdate = 9;
-	}
-	if (month == 6)
-	{
-		doomsdate = 6;
-	}
-	if (month == 7)
-	{
-		doomsdate = 11;
-	}
-	if (month == 8)
-	{
-		doomsdate = 8;
-	}
-	if (month == 9)
-	{
-		doomsdate = 5;
-	}
-	if (month == 10)
-	{
-		doomsdate = 10;
-	}
-	if (month == 11)
-	{
-		doomsdate = 7;
-	}
-	if (month == 12)
-	{
-		doomsdate = 12;
-	}
-	printf("%d is doomsdate\n", doomsdate);
-	
-	//faut kisspasskekchose icitte
-	dayOfWeek = ((doomsdate - day) % 7) - doomsday;
-	printf("%d is day of week\n", dayOfWeek);
-
-	return (dayOfWeek);
-}
-
-//First program
-//Check if leap year
-//26/01/2014
-
-/**#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-#define StartOfGreg 1582
-
-int isLeapYear(int year);
-
-int main(int argc, char* argv[])
-{
-	int year;
-
-	printf("Enter a Year\n");
-	scanf("%d", &year);
-
-	//debut du calendrier greg
-	//pas de leap year avant
-	assert(Year >= StartOfGreg);
-
-	//check true or false 
-	if (isLeapYear(year) == 1) {
-		printf("%d is a leap year\n", Year);
-	}
-	else
-	{
-		printf("%d is not a leap year\n", year);
-	}
-	return EXIT_SUCCESS;
-}**/
-
-int isLeapYear(int year)
-{
-
-	int leapYear;
-
-	if ((year % 4) == 0) {
-		if ((year % 100) == 0) 
-		{
-			if ((year % 400) == 0) 
-			{
-				leapYear = TRUE;
-			}
-			else
-			{
-				leapYear = FALSE;
-			}
-		}
-		else
-		{
-			leapYear = TRUE;
-		}
-	}
-	else
-	{
-		leapYear = FALSE;
-	}
-	return leapYear;
-}
-/**int zedoomdate(int month, int leapYear)
-{
-
-	int doomsdate;
-	
 
 	if (month == 1)
 	{
@@ -294,7 +167,64 @@ int isLeapYear(int year)
 	{
 		doomsdate = 12;
 	}
-	printf("%d", doomsdate);
-		return doomsdate;
-}**/
+	printf("%d is doomsdate\n", doomsdate);
+	
+	//faut kisspasskekchose icitte
+	if ((doomsday - ((doomsdate - day) % 7)) > 0)
+	{
+		dayOfWeek = (doomsday - ((doomsdate - day)) % 7 );
+		printf("+\n");
+	}
+	else if ((doomsdate - day) == 0)
+	{
+		dayOfWeek = ((doomsday + ((day - doomsdate) % 7)) % 7);
+		printf("-\n");
+	}
+
+	else
+	{
+		dayOfWeek = ((doomsday + ((day - doomsdate)%7)) % 7 ) + 7;
+		printf("-\n");
+	}
+
+	if (dayOfWeek > 6)
+	{
+		dayOfWeek = dayOfWeek - 7 ;
+	}
+	//dayOfWeek = (doomsday - (doomsdate - day)) % 7;
+	printf("%d is day of week\n\n", dayOfWeek);
+
+	return (dayOfWeek);
+
+}
+
+
+int isLeapYear(int year)
+{
+
+	int leapYear;
+
+	if ((year % 4) == 0) {
+		if ((year % 100) == 0) 
+		{
+			if ((year % 400) == 0) 
+			{
+				leapYear = TRUE;
+			}
+			else
+			{
+				leapYear = FALSE;
+			}
+		}
+		else
+		{
+			leapYear = TRUE;
+		}
+	}
+	else
+	{
+		leapYear = FALSE;
+	}
+	return leapYear;
+}
 
